13fa00869ffaf79813c57f8fdfa54e6a66ae1de3
Lukasz Guziak <guziak.luk@gmail.com>
2022-11-23 15:24:51 +0100

    add functions addNewTodo createTodoTools

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,66 @@
 const inputTodo = document.querySelector('.header__input');
 const addTodoBtn = document.querySelector('.header__add-btn');
-const todoListInfo = document.querySelector('.todo-list__info');
+const todoListInfo = document.querySelector('.todo-lists__info');
 const workList = document.querySelector('#work-list');
 const popup = document.querySelector('.popup');
 const popupInput = document.querySelector('.popup__input');
 const popupAcceptBtn = document.querySelector('.accept');
 const popupCancelBtn = document.querySelector('.cancel');
 const popupInfo = document.querySelector('.popup__info');
+let idNumber = 0;
+let newTodo;
+let mainBox;
 console.log(workList);
 console.log(todoListInfo.innerHTML);
 console.log('test2');
 const addNewTodo = () => {
-    console.log('dodanie nowego todo');
+    let inputTodoValue = inputTodo.value;
+    if (inputTodoValue !== '') {
+        newTodo = document.createElement('li');
+        newTodo.classList.add('todo');
+        newTodo.setAttribute('id', `todo-${idNumber}`);
+        mainBox = document.createElement('div');
+        mainBox.classList.add('main-box');
+        const todoText = document.createElement('p');
+        todoText.classList.add('todo__text');
+        todoText.innerText = `${inputTodoValue}`;
+        mainBox.appendChild(todoText);
+        createTodoTools();
+        // const todoTools = document.createElement('div');
+        // todoTools.classList.add('todo__tools');
+        newTodo.appendChild(mainBox);
+        console.log('dodanie nowego todo');
+        console.log(newTodo);
+        workList.appendChild(newTodo);
+        todoListInfo.innerText = '';
+        inputTodo.value = '';
+        idNumber++;
+    }
+    else {
+        todoListInfo.innerText = 'Wpisz treść zadania';
+    }
+};
+const createTodoTools = () => {
+    const todoTools = document.createElement('div');
+    todoTools.classList.add('todo__tools');
+    mainBox.appendChild(todoTools);
+    const addBtn = document.createElement('button');
+    addBtn.classList.add('add');
+    addBtn.innerHTML = '<i class="fas fa-plus"></i>';
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('eddit');
+    editBtn.innerHTML = 'EDIT';
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+    todoTools.appendChild(addBtn);
+    todoTools.appendChild(completeBtn);
+    todoTools.appendChild(editBtn);
+    todoTools.appendChild(deleteBtn);
+    console.log(todoTools);
 };
 function getDirection(e) {
     let directionCode = e.keyCode;
@@ -24,8 +73,25 @@ const enterCheck = (e) => {
         addNewTodo();
     }
 };
+const checkClick = (e) => {
+    if (e.target.tagName === 'BUTTON' || e.target.tagName === 'I') {
+        if (e.target.closest('button').classList.contains('add')) {
+            console.log('add');
+        }
+        if (e.target.closest('button').classList.contains('complete')) {
+            console.log('COMPLETE !!!');
+        }
+        if (e.target.closest('button').classList.contains('eddit')) {
+            console.log('edit !!!');
+        }
+        if (e.target.closest('button').classList.contains('delete')) {
+            console.log('delete');
+        }
+    }
+};
 addTodoBtn.addEventListener('click', addNewTodo);
 inputTodo.addEventListener('keyup', enterCheck);
+workList.addEventListener('click', checkClick);
 // <li class="todo" id="test2">
 // <div class="main-box">
 //     <p class="todo__text">zrobić apkę todo</p>
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,7 +3,7 @@ const addTodoBtn: HTMLButtonElement =
 	document.querySelector('.header__add-btn');
 
 const todoListInfo: HTMLParagraphElement =
-	document.querySelector('.todo-list__info');
+	document.querySelector('.todo-lists__info');
 
 const workList: HTMLUListElement = document.querySelector('#work-list');
 
@@ -13,12 +13,75 @@ const popupAcceptBtn: HTMLButtonElement = document.querySelector('.accept');
 const popupCancelBtn: HTMLButtonElement = document.querySelector('.cancel');
 const popupInfo: HTMLParagraphElement = document.querySelector('.popup__info');
 
+let idNumber: number = 0;
+let newTodo: HTMLLIElement;
+let mainBox: HTMLDivElement;
+
 console.log(workList);
 console.log(todoListInfo.innerHTML);
 console.log('test2');
 
 const addNewTodo = () => {
-	console.log('dodanie nowego todo');
+	let inputTodoValue: string = inputTodo.value;
+
+	if (inputTodoValue !== '') {
+		newTodo = document.createElement('li');
+		newTodo.classList.add('todo');
+		newTodo.setAttribute('id', `todo-${idNumber}`);
+
+		mainBox = document.createElement('div');
+		mainBox.classList.add('main-box');
+
+		const todoText = document.createElement('p');
+		todoText.classList.add('todo__text');
+		todoText.innerText = `${inputTodoValue}`;
+		mainBox.appendChild(todoText);
+		createTodoTools();
+
+		// const todoTools = document.createElement('div');
+		// todoTools.classList.add('todo__tools');
+
+		newTodo.appendChild(mainBox);
+		console.log('dodanie nowego todo');
+		console.log(newTodo);
+
+		workList.appendChild(newTodo);
+
+		todoListInfo.innerText = '';
+		inputTodo.value = '';
+		idNumber++;
+	} else {
+		todoListInfo.innerText = 'Wpisz treść zadania';
+	}
+};
+
+const createTodoTools = () => {
+	const todoTools = document.createElement('div');
+	todoTools.classList.add('todo__tools');
+	mainBox.appendChild(todoTools);
+
+	const addBtn = document.createElement('button');
+	addBtn.classList.add('add');
+	addBtn.innerHTML = '<i class="fas fa-plus"></i>';
+
+	const completeBtn = document.createElement('button');
+	completeBtn.classList.add('complete');
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+	const editBtn = document.createElement('button');
+	editBtn.classList.add('edit');
+	editBtn.innerHTML = 'EDIT';
+
+	const deleteBtn = document.createElement('button');
+	deleteBtn.classList.add('delete');
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+	todoTools.appendChild(addBtn);
+	todoTools.appendChild(completeBtn);
+	todoTools.appendChild(editBtn);
+	todoTools.appendChild(deleteBtn);
+
+	console.log(todoTools);
 };
 
 function getDirection(e: KeyboardEvent): void {
@@ -33,9 +96,28 @@ const enterCheck = (e: KeyboardEvent): void => {
 		addNewTodo();
 	}
 };
+const checkClick = (e) => {
+
+
+	if (e.target.tagName==='BUTTON' || e.target.tagName==='I') {
+		if (e.target.closest('button').classList.contains('add')) {
+			console.log('add');
+		}
+		if (e.target.closest('button').classList.contains('complete')) {
+			console.log('COMPLETE !!!');
+		}
+		if (e.target.closest('button').classList.contains('edit')) {
+			console.log('edit !!!');
+		}
+		if (e.target.closest('button').classList.contains('delete')) {
+			console.log('delete');
+		}
+	}
+};
 
 addTodoBtn.addEventListener('click', addNewTodo);
 inputTodo.addEventListener('keyup', enterCheck);
+workList.addEventListener('click', checkClick);
 
 // <li class="todo" id="test2">
 
@@ -46,7 +128,7 @@ inputTodo.addEventListener('keyup', enterCheck);
 //     <div class="todo__tools">
 //         <button class="add"><i class="fas fa-plus"></i></button>
 //         <button class="complete"><i class="fas fa-check"></i></button>
-//         <button class="eddit">EDIT</button>
+//         <button class="edit">EDIT</button>
 //         <button class="delete"><i class="fas fa-times"></i></button>
 //     </div>
 
@@ -56,7 +138,7 @@ inputTodo.addEventListener('keyup', enterCheck);
 //     <li class="todo-sub" id="sub2">podpunkt 2.1
 //         <div class="todo-sub__tools">
 //             <button class="complete"><i class="fas fa-check"></i></button>
-//             <button class="eddit">EDIT</button>
+//             <button class="edit">EDIT</button>
 //             <button class="delete"><i class="fas fa-times"></i></button>
 //         </div>
 //     </li>
